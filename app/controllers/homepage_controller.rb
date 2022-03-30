e87731da680b8ca9086889e04935df68dfc00fcf
Kristina Peru <krissy.germanov@gmail.com>
2022-03-30 04:09:06 -0600

    homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,8 +3,7 @@ class HomepageController < ApplicationController
     get '/' do
         books = Book.all.limit(7)
         books.to_json(
-            only: [:title, :image],
-            include: [:author, :genre]
+            only: [:image],
         )
     end
 end
